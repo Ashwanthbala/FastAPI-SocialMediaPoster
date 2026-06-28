514e7bce89ee79ebe665992711cb891c65ff52bb
AshwanthBala <ashwanthbalajir@gmail.com>
2026-06-28 18:02:06 +0530

    Initial Commit message

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
+from app.schemas import PostCreate
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+from sqlalchemy import select
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...), caption: str = Form(""), session: AsyncSession = Depends(get_async_session)):
+    post = Post(caption=caption,url="dummyurl",file_type="photo",file_name="dummy name")
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    pass
